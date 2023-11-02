a71f1a3058c8c4a7ddcaa6847f7628351c689352
Elliot59 <elliot.59@outlook.com>
2023-11-02 18:08:21 +0600

    code formatted

--- book/models.py
+++ book/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
-# Create your models here.
 
 class Author(models.Model):
     name = models.CharField(max_length=30, blank=True, null=True)
 
     def __str__(self):
         return self.name
+
 
 class Book(models.Model):
     title = models.CharField(max_length=50, blank=True, null=True)
