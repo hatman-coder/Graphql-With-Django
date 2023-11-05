bb150e9820881ae9370ddef93b86035839a552fc
Elliot59 <elliot.59@outlook.com>
2023-11-05 18:09:28 +0600

    filter functionality added for schema

--- book/schema.py
+++ book/schema.py
@@ -1,7 +1,8 @@
 import graphene
-from graphene import ObjectType
+from graphene import ObjectType, relay
 from graphene_django import DjangoObjectType
 from .models import Book, Author
+from graphene_django.filter import DjangoFilterConnectionField
 
 # Define the AuthorType and BookType with their fields and connections
 class AuthorType(DjangoObjectType):
@@ -13,6 +14,11 @@ class BookType(DjangoObjectType):
     class Meta:
         model = Book
         fields = ['id', 'title', 'author', 'brief', 'published_year']
+        filter_fields = {
+            'title': ['exact', 'icontains'],
+            'id': ['exact']
+        }
+        interfaces = (relay.Node, )
 
     author = graphene.Field(AuthorType)
 
@@ -42,10 +48,10 @@ class CreateBook(graphene.Mutation):
 
 # Define a query for retrieving all books
 class Query(ObjectType):
-    all_books = graphene.List(BookType)
+    all_books = DjangoFilterConnectionField(BookType)
 
-    def resolve_all_books(self, info):
-        return Book.objects.all()
+    def resolve_all_books(self, info, **kwargs):
+        return Book.objects.filter(**kwargs)
 
 # Define the mutation for creating a book
 class Mutation(ObjectType):
