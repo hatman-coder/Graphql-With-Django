437e17c465c5b5a4b8c8c92b5b478c7a52da6c97
Elliot59 <elliot.59@outlook.com>
2023-11-05 18:09:45 +0600

    Model admiin added for book

--- book/admin.py
+++ book/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import Book, Author
 
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title']
+
 # Register your models here.
-admin.site.register(Book)
+admin.site.register(Book, BookAdmin)
 admin.site.register(Author)
